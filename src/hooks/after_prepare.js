31177da39592b295a0359949969796d34e0cf4be
Miguel Antunes <miguel.aka.kelter@gmail.com>
2023-07-10 18:36:06 +0100

    test

--- src/hooks/after_prepare.js
+++ src/hooks/after_prepare.js
@@ -1,67 +1,37 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require("path");
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
-var semver = require('semver');
-var builder = new xml2js.Builder({
-    xmldec: {
-        version: '1.0',
-        encoding: 'UTF-8'
-    }
-});
+module.exports = function(context) {
 
-module.exports = function (context) {
+    var fs = require('fs'),
+        path = require('path');
+  
+    var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+    var manifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
+  
+    console.log("Platform ROOT: " + platformRoot);
+    console.log("Manifest file: " + manifestFile);
+  
+    // If manifest file exists
+    if (fs.existsSync(manifestFile)) {
+      console.log("Manifest file exists");
+  
+      // Read manifest file
+      fs.readFile(manifestFile, 'utf8', function (err, data) {
+        if (err) {
+          throw new Error('Unable to find AndroidManifest.xml: ' + err);
+        }
+  
+        data = data.replace('sensorLandscape', 'fullSensor');
 
-    if(context.opts.platforms.indexOf('android') === -1) return;
-
-    console.log('Attempting to set app name');
-
-    var projectRoot = context.opts.projectRoot;
-
-    const usesNewStructure = fs.existsSync(path.join(projectRoot, 'platforms', 'android', 'app'));
-    const basePath = usesNewStructure ? path.join(projectRoot, 'platforms', 'android', 'app', 'src', 'main') : path.join(projectRoot, 'platforms', 'android');
-    var configPath = path.join(basePath, 'res', 'xml', 'config.xml');
-    var stringsPath = path.join(basePath, 'res', 'values', 'strings.xml');
-    var stringsXml, name;
-
-    // make sure the android config file exists
-    try {
-        fs.accessSync(configPath, fs.F_OK);
-    } catch(e) {
-        console.error(`Could not find android config.xml at ${configPath}`);
-        return;
-    }
-
-    name = getConfigParser(context, configPath).getPreference('AppName');
-
-    if (name) {
-        stringsXml = fs.readFileSync(stringsPath, 'UTF-8');
-        parser.parseString(stringsXml, function (err, data) {
-
-            data.resources.string.forEach(function (string) {
-
-                if (string.$.name === 'app_name') {
-
-                    console.log('Setting App Name: ', name);
-                    string._ = name;
-                }
-            });
-
-            fs.writeFileSync(stringsPath, builder.buildObject(data));
-
-        });
-    }
-};
-
-function getConfigParser(context, config) {
-
-    if (semver.lt(context.opts.cordova.version, '5.4.0')) {
-        ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+  
+        // Replace manifest file with updated version
+        if(data){
+          fs.writeFile(manifestFile, data, 'utf8', function (err) {
+            if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
+          })
+        }
+      });
     } else {
-        ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+      console.log("Manifest file DOES NOT exist");
     }
-
-    return new ConfigParser(config);
-}
+  };
