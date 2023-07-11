a279c12c3cbce529eb1fcadcef98654278edc75c
Miguel Antunes <miguel.aka.kelter@gmail.com>
2023-07-11 08:56:48 +0100

    Update after_prepare.js

--- src/hooks/after_prepare.js
+++ src/hooks/after_prepare.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
 
-module.exports = function(context) {
+const fs = require('fs');
+const path = require('path');
 
-    var fs = require('fs'),
-        path = require('path');
-  
-    var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
-    var manifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
-  
-    console.log("Platform ROOT: " + platformRoot);
-    console.log("Manifest file: " + manifestFile);
-  
-    // If manifest file exists
-    if (fs.existsSync(manifestFile)) {
-      console.log("Manifest file exists");
-  
-      // Read manifest file
-      fs.readFile(manifestFile, 'utf8', function (err, data) {
+module.exports = function (context) {
+  const platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+  const manifestFile = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
+
+  console.log("Platform Root:", platformRoot);
+  console.log("Manifest File:", manifestFile);
+
+  // Check if manifest file exists
+  if (fs.existsSync(manifestFile)) {
+    console.log("Manifest file exists");
+
+    // Read manifest file
+    fs.readFile(manifestFile, 'utf8', function (err, data) {
+      if (err) {
+        throw new Error('Unable to read AndroidManifest.xml: ' + err);
+      }
+
+      // Replace 'sensorLandscape' with 'fullSensor'
+      data = data.replace(/sensorLandscape/g, 'fullSensor');
+
+      // Write updated manifest file
+      fs.writeFile(manifestFile, data, 'utf8', function (err) {
         if (err) {
-          throw new Error('Unable to find AndroidManifest.xml: ' + err);
+          throw new Error('Unable to write AndroidManifest.xml: ' + err);
         }
-  
-        data = data.replace('sensorLandscape', 'fullSensor');
-
-  
-        // Replace manifest file with updated version
-        if(data){
-          fs.writeFile(manifestFile, data, 'utf8', function (err) {
-            if (err) throw new Error('Unable to write AndroidManifest.xml: ' + err);
-          })
-        }
+        console.log("AndroidManifest.xml updated successfully!");
       });
-    } else {
-      console.log("Manifest file DOES NOT exist");
-      throw new Error('Unable to find AndroidManifest.xml: ');
-    }
-  };
+    });
+  } else {
+    console.log("Manifest file does not exist");
+    throw new Error('Unable to find AndroidManifest.xml');
+  }
+};
